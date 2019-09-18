aab5c96656c9cdc3f3182a6bf91d5abe474ba609
rajasimhan varadharajan <rvaradharajan@corelogic.com>
2019-09-18 12:45:45 -0500

    Initial commit
    - This is a dummy child project that is loaded via angular elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {Injector, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {DashboardComponent} from '../dashboard/dashboard.component';
+import {createCustomElement} from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [DashboardComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector){}
+
+  ngDoBootstrap(){
+    const custom = createCustomElement(DashboardComponent, {injector: this.injector});
+    customElements.define("app-rct", custom);
+  }
+}
---
+++ src/dashboard/dashboard.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rct',
+  templateUrl: './dashboard.component.html',
+})
+export class DashboardComponent {
+}
